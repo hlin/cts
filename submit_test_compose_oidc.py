5ebbf60fef82436a222e34b4055ad4aa1779e649
Ralph Bean <rbean@redhat.com>
2017-09-26 12:38:26 -0400

    Add a test script for submitting oidc composes.
    This doesn't quite work yet.  I get a 401 unauthorized error.  Not sure
    yet if the issue is client side or server side.

---
+++ submit_test_compose_oidc.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import openidc_client
+import requests.exceptions
+
+import odcs.client.odcs
+
+# Get the auth token using the OpenID client.
+id_provider = 'https://id.stg.fedoraproject.org/openidc/'
+oidc = openidc_client.OpenIDCClient(
+    'odcs',
+    id_provider,
+    {'Token': 'Token', 'Authorization': 'Authorization'},
+    'odcs-authorizer',
+    'notsecret',
+)
+
+scopes = [
+    'openid',
+    'https://id.fedoraproject.org/scope/groups',
+    'https://pagure.io/odcs/new-compose',
+]
+try:
+    token = oidc.get_token(scopes, new_token=True)
+except requests.exceptions.HTTPError as e:
+    print e.response.text
+    raise
+
+client = odcs.client.odcs.ODCS(
+    'https://odcs.stg.fedoraproject.org',
+    auth_mech=odcs.client.odcs.AuthMech.OpenIDC,
+    openidc_token=token,
+)
+
+result = client.new_compose(
+    source='module-base-runtime-master-20170313200124',
+    source_type='tag',
+    packages=['libselinux'],
+    flags=['nodeps'],
+)
+print result
