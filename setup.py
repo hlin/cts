1c253784aac3947e0c09056e352658c20f2eb31c
Ralph Bean <rbean@redhat.com>
2017-09-29 10:52:53 -0400

    0.0.7

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('test-requirements.txt') as f:
 
 setup(name='odcs',
       description='On Demand Compose Service',
-      version='0.0.6',
+      version='0.0.7',
       classifiers=[
           "Programming Language :: Python",
           "Topic :: Software Development :: Build Tools"
