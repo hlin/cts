128383a5c97032e950afde785b99b3e5889a59d4
Jan Kaluza <jkaluza@redhat.com>
2018-03-12 14:08:28 +0100

    Version 0.2.1

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ with open('test-requirements.txt') as f:
 
 setup(name='odcs',
       description='On Demand Compose Service',
-      version='0.2.0',
+      version='0.2.1',
       classifiers=[
           "Programming Language :: Python",
           "Topic :: Software Development :: Build Tools"
