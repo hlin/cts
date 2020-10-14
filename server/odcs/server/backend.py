9388f2a9e4f730c2168749e4c9f87ed987d2a387
Lubomír Sedlář <lsedlar@redhat.com>
2020-10-14 10:20:25 +0200

    Break too long line
    Signed-off-by: Lubomír Sedlář <lsedlar@redhat.com>

--- server/odcs/server/backend.py
+++ server/odcs/server/backend.py
@@ -220,7 +220,10 @@ class RemoveExpiredComposesThread(BackendThread):
 
             # Do not remove the latest successfull compose.
             if get_latest_symlink(compose):
-                log.info("%r: Not removing compose, the latest-* symlink points to it.", compose)
+                log.info(
+                    "%r: Not removing compose, the latest-* symlink points to it.",
+                    compose,
+                )
                 continue
 
             log.info("%r: Removing compose", compose)
