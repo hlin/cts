063272193b20c25e7a5704c83c7c6547e6cd0f5e
Ozan Unsal <ounsal@redhat.com>
2021-11-19 15:08:44 +0100

    Delete copr since it is not used anymore
    Fixes: https://pagure.io/odcs/issue/421
    Jira: RHELCMP-1471
    
    Signed-off-by: Ozan Unsal <ounsal@redhat.com>

--- .copr/prepare_spec.sh
+++
@@ -1,5 +0,0 @@
-dnf -y install python git fedpkg python-setuptools
-ODCS_VERSION=$(python setup.py -V)
-ODCS_RELEASE=$(git log -1 --pretty=format:%ct)
-sed -e "s|\$ODCS_VERSION|$ODCS_VERSION|g" \
-        -e "s|\$ODCS_RELEASE|$ODCS_RELEASE|g" ./.copr/odcs.spec.in > ./.copr/odcs.spec;
